6cbd287ddc0568bc77ac229e8b9dc2837a968dcd
Aure6 <aurelien7112001@gmail.com>
2023-10-13 14:07:07 +0200

    SVGRenderer commenté

--- src/Renderers/SVGRenderer.php
+++ src/Renderers/SVGRenderer.php
@@ -2,10 +2,11 @@
 
 namespace Opmvpc\Formes\Renderers;
 
+// Importing classes and namespaces
 use Opmvpc\Formes\Canvas;
 use Opmvpc\Formes\Rectangle;
-use Opmvpc\Formes\Cercle; 
-use Opmvpc\Formes\Ligne; 
+use Opmvpc\Formes\Cercle;
+use Opmvpc\Formes\Ligne;
 use Opmvpc\Formes\Polygone;
 use SVG\Nodes\Shapes\SVGCircle;
 use SVG\Nodes\Shapes\SVGLine;
@@ -13,18 +14,28 @@ use SVG\Nodes\Shapes\SVGPolygon;
 use SVG\Nodes\Shapes\SVGRect;
 use SVG\SVG;
 
+// Defining the SVGRenderer class implementing the Renderer interface
 class SVGRenderer implements Renderer {
+    // Declaring a private variable of type Canvas
     private Canvas $canvas;
 
+    // Constructor of the SVGRenderer class
     public function __construct(Canvas $canvas) {
         $this->canvas = $canvas;
     }
+
+    // Function to render the SVG image as a string
     public function render(): string {
+        // Creating a new SVG image with the width and height of the canvas
         $image = new SVG($this->canvas->getWidth(), $this->canvas->getHeight());
+        // Getting the SVG document for further manipulations
         $doc = $image->getDocument();
         
+        // Creating a rectangle that covers the whole canvas
         $square = new SVGRect(0, 0, $this->canvas->getWidth(), $this->canvas->getHeight(), $this->canvas->getCouleur());
+        // Setting the fill color of the SVG document
         $doc->setStyle('fill', $this->canvas->getCouleur());
+        // Adding the rectangle to the SVG document
         $doc->addChild($square);
         
         //partie ajoutée
@@ -40,29 +51,42 @@ class SVGRenderer implements Renderer {
                     break;
             }
         } */
+        // Iterating over the formes in the canvas
         foreach ($this->canvas->getFormes() as $forme) {
+            // Switching based on the class of the forme
             switch (get_class($forme)) {
+                // If the forme is a Rectangle
                 case Rectangle::class:
                     $rect = new SVGRect($forme->getPoint1()->getX(), $forme->getPoint2()->getY(), $forme->getWidth(), $forme->getHeight());
                     $rect->setStyle('fill', $forme->getCouleur());
+                    // Adding the rectangle to the SVG document
                     $doc->addChild($rect);
                     break;
+                // If the forme is a Line
                 case Ligne::class:
                     $line = new SVGLine($forme->getPoint1()->getX(), $forme->getPoint2()->getY());
                     $line->setStyle('stroke', $forme->getCouleur());
+                    // Adding the line to the SVG document
                     $doc->addChild($line);
                     break;
+                // If the forme is a Cercle
                 case Cercle::class:
-                    // Implement Cercle rendering here
+                    // TODO: Implement Cercle rendering
                     break;
+                // If the forme is a Polygone
                 case Polygone::class:
-                    // Implement Polygone rendering here
+                    // TODO: Implement Polygone rendering
                     break;
             }
         }
         //fin partie ajoutée
-         
-        return $image->toXMLString();
+
+       // Returning the SVG image as a string
+       return $image->toXMLString();
     }
-    public function save(string $path): void {}
+
+    // Function to save the SVG image to a file
+    public function save(string $path): void {
+        // TODO: Implement the save functionality
+    }
 }
